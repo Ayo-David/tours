a8bb02a1c98a890dc9a1a4dbe8a50c375820e16e
Ayo-David Ajayi <ayodavid.ajayi@gmail.com>
2021-11-12 16:46:40 +0100

    Food Menu Completed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import App from './AppMenu';
+import App from './AppGrocery';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
