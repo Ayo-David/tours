85c50a5132050de7349031f4fc0c605279c8ce6a
Ayo-David Ajayi <ayodavid.ajayi@gmail.com>
2022-06-23 17:42:10 +0100

    submenu AppJS

---
+++ src/AppSubMenu.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './SubMenuHome'
+import NavBar from './SubMenuNavBar'
+import Sidebar from './SubMenuSidebar'
+
+const App = () => {
+    return (
+        <>
+            <Home />
+            <NavBar />
+            <Sidebar />
+        </>
+    )
+}
+
+export default App
